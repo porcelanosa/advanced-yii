c552da4e4de8dbc5ba26e52230cad97bbeb84e6b
porcelanosa <public_mce@rambler.ru>
2017-11-04 01:48:13 +0300

    init first stage

---
+++ common/config/main.php
@@ -0,0 +1,27 @@
+<?php
+return [
+    'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
+    
+    // set target language to be Russian
+    'language' => 'ru-RU',
+    // set source language to be English
+    'sourceLanguage' => 'en-US',
+    
+    'components' => [
+        'cache' => [
+            'class' => 'yii\caching\FileCache',
+        ],
+    ],
+    'modules' => [
+        'user' => [
+            'class' => 'dektrium\user\Module',
+            // you will configure your module inside this file
+            // or if need different configuration for frontend and backend you may
+            // configure in needed configs
+            'enableUnconfirmedLogin' => true,
+            'confirmWithin' => 21600,
+            'cost' => 12,
+            'admins' => ['admin']
+        ],
+    ],
+];
