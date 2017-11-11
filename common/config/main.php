d8178e6c214da2cc676b1935a27c28a198f64d1f
porcelanosa <public_mce@rambler.ru>
2017-11-11 20:26:52 +0300

    Add COOKIE_VALIDATION_KEY

--- common/config/main.php
+++ common/config/main.php
@@ -18,6 +18,10 @@
             'cache' => [
                 'class' => 'yii\caching\FileCache',
             ],
+            'request' => [
+                // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
+                'cookieValidationKey' => getenv('COOKIE_VALIDATION_KEY'),
+            ],
         ],
         'modules'        => [
             'user' => [
