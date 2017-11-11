e3201f322367fed79c3ee67c3bcd1a6cac3f26c4
porcelanosa <public_mce@rambler.ru>
2017-11-11 15:23:36 +0300

    Fix backend config

--- backend/config/main.php
+++ backend/config/main.php
@@ -10,7 +10,7 @@
         'id'                  => 'app-backend',
         'basePath'            => dirname(__DIR__),
         'controllerNamespace' => 'backend\controllers',
-        'bootstrap'           => ['log', 'porcelanosa\posts\Bootstrap'],
+        'bootstrap'           => ['log'],
         
         'controllerMap' => [
             'elfinder' => [
@@ -43,107 +43,106 @@
                     ]*/
                 
                 ],
-                'bootstrap'        => ['log'],
-                'modules'          => [
-                    'user'  => [
-                        // following line will restrict access to profile, recovery, registration and settings controllers from backend
-                        'as backend' => 'dektrium\user\filters\BackendFilter',
-                    ],
-                    'rbac'  => 'dektrium\rbac\RbacWebModule',
-                    'posts' => [
-                        'class'            => 'porcelanosa\posts\Module',
-                        'image_url'        => '/images/posts',
-                        'image_path_alias' => Yii::getAlias('@frontend') . '/web/images/posts',
-                        'controllerMap'    => [
-                            'postadmin' => [
-                                'class'     => 'porcelanosa\posts\controllers\AdminController',
-                                'as access' => [
-                                    'class' => \yii\filters\AccessControl::className(),
-                                    'rules' => [
-                                        [
-                                            'allow' => true,
-                                            'roles' => ['administrator'],
-                                        ],
-                                    ],
+            ]
+        ],
+        'modules'       => [
+            'user'  => [
+                // following line will restrict access to profile, recovery, registration and settings controllers from backend
+                'as backend' => 'dektrium\user\filters\BackendFilter',
+            ],
+            'rbac'  => 'dektrium\rbac\RbacWebModule',
+            'posts' => [
+                'class'            => 'porcelanosa\posts\Module',
+                'image_url'        => '/images/posts',
+                'image_path_alias' => Yii::getAlias('@frontend') . '/web/images/posts',
+                'controllerMap'    => [
+                    'postadmin' => [
+                        'class'     => 'porcelanosa\posts\controllers\AdminController',
+                        'as access' => [
+                            'class' => \yii\filters\AccessControl::className(),
+                            'rules' => [
+                                [
+                                    'allow' => true,
+                                    'roles' => ['administrator'],
                                 ],
                             ],
                         ],
                     ],
-                
                 ],
-                'components'       => [
-                    'request'      => [
-                        'baseUrl'   => '/admin',
-                        'csrfParam' => '_csrf-backend',
-                    ],
-                    /*'user' => [
-                        'identityClass' => 'common\models\User',
-                        'enableAutoLogin' => true,
-                        'identityCookie' => ['name' => '_identity-backend', 'httpOnly' => true],
-                    ],
-                    'session' => [
-                        // this is the name of the session cookie used for login on the backend
-                        'name' => 'advanced-backend',
-                    ],*/
-                    'user'         => [
-                        'identityCookie' => [
-                            'name'     => '_backendIdentity',
-                            'path'     => '/admin',
-                            'httpOnly' => true,
-                        ],
-                    ],
-                    'session'      => [
-                        'name'         => 'BACKENDSESSID',
-                        'cookieParams' => [
-                            'httpOnly' => true,
-                            'path'     => '/admin',
-                        ],
-                    ],
-                    'log'          => [
-                        'traceLevel' => YII_DEBUG ? 3 : 0,
-                        'targets'    => [
-                            [
-                                'class'  => 'yii\log\FileTarget',
-                                'levels' => ['error', 'warning'],
-                            ],
-                        ],
-                    ],
-                    'errorHandler' => [
-                        'errorAction' => 'site/error',
-                    ],
-                    'urlManager'   => [
-                        'scriptUrl'       => '/admin/index.php',
-                        'enablePrettyUrl' => true,
-                        'showScriptName'  => false,
-                        'rules'           => [
-                        ],
-                    ],
-                    /*
-                    'urlManager' => [
-                        'enablePrettyUrl' => true,
-                        'showScriptName' => false,
-                        'rules' => [
-                        ],
-                    ],
-                    */
-                    /*'view' => [
-                        'theme' => [
-                            'pathMap' => [
-                                '@app/views' => '@vendor/dmstr/yii2-adminlte-asset/example-views/yiisoft/yii2-app'
-                            ],
-                        ],
-                    ],*/
-                    'assetManager' => [
-                        'bundles' => [
-                            'dmstr\web\AdminLteAsset' => [
-                                'skin' => 'skin-black-light', // тема для AdminLTE
-                            ],
-                        ],
+            ],
+        
+        ],
+        'components'    => [
+            'request'      => [
+                'baseUrl'   => '/admin',
+                'csrfParam' => '_csrf-backend',
+            ],
+            /*'user' => [
+                'identityClass' => 'common\models\User',
+                'enableAutoLogin' => true,
+                'identityCookie' => ['name' => '_identity-backend', 'httpOnly' => true],
+            ],
+            'session' => [
+                // this is the name of the session cookie used for login on the backend
+                'name' => 'advanced-backend',
+            ],*/
+            'user'         => [
+                'identityCookie' => [
+                    'name'     => '_backendIdentity',
+                    'path'     => '/admin',
+                    'httpOnly' => true,
+                ],
+            ],
+            'session'      => [
+                'name'         => 'BACKENDSESSID',
+                'cookieParams' => [
+                    'httpOnly' => true,
+                    'path'     => '/admin',
+                ],
+            ],
+            'log'          => [
+                'traceLevel' => YII_DEBUG ? 3 : 0,
+                'targets'    => [
+                    [
+                        'class'  => 'yii\log\FileTarget',
+                        'levels' => ['error', 'warning'],
                     ],
                 ],
-                'params'           => $params,
-            ]
-        ]
+            ],
+            'errorHandler' => [
+                'errorAction' => 'site/error',
+            ],
+            'urlManager'   => [
+                'scriptUrl'       => '/admin/index.php',
+                'enablePrettyUrl' => true,
+                'showScriptName'  => false,
+                'rules'           => [
+                ],
+            ],
+            /*
+            'urlManager' => [
+                'enablePrettyUrl' => true,
+                'showScriptName' => false,
+                'rules' => [
+                ],
+            ],
+            */
+            /*'view' => [
+                'theme' => [
+                    'pathMap' => [
+                        '@app/views' => '@vendor/dmstr/yii2-adminlte-asset/example-views/yiisoft/yii2-app'
+                    ],
+                ],
+            ],*/
+            'assetManager' => [
+                'bundles' => [
+                    'dmstr\web\AdminLteAsset' => [
+                        'skin' => 'skin-black-light', // тема для AdminLTE
+                    ],
+                ],
+            ],
+        ],
+        'params'        => $params,
     ];
     
     if (YII_ENV_DEV) {
