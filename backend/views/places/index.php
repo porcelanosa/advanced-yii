62e4db37847351f0e9185253364fd326d92a0b87
porcelanosa <public_mce@rambler.ru>
2017-11-13 17:26:52 +0300

    Places admin some changes

--- backend/views/places/index.php
+++ backend/views/places/index.php
@@ -1,19 +1,20 @@
 <?php
-
-use yii\helpers\Html;
-use yii\grid\GridView;
-use yii\widgets\Pjax;
-/* @var $this yii\web\View */
-/* @var $searchModel common\models\search\PlacesSearch */
-/* @var $dataProvider yii\data\ActiveDataProvider */
-
-$this->title = 'Places';
-$this->params['breadcrumbs'][] = $this->title;
+    
+    use yii\grid\GridView;
+    use yii\helpers\Html;
+    use yii\widgets\Pjax;
+    
+    /* @var $this yii\web\View */
+    /* @var $searchModel common\models\search\PlacesSearch */
+    /* @var $dataProvider yii\data\ActiveDataProvider */
+    
+    $this->title                   = 'Places';
+    $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="places-index box box-primary">
     <?php Pjax::begin(); ?>
     <div class="box-header with-border">
-        <?= Html::a('Create Places', ['create'], ['class' => 'btn btn-success btn-flat']) ?>
+        <?=Html::a('Create Places', ['create'], ['class' => 'btn btn-success btn-flat'])?>
     </div>
     <div class="box-body table-responsive no-padding">
         <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
@@ -22,14 +23,45 @@ $this->params['breadcrumbs'][] = $this->title;
             'filterModel' => $searchModel,
             'layout' => "{items}\n{summary}\n{pager}",
             'columns' => [
-                ['class' => 'yii\grid\SerialColumn'],
-
-                'id',
-                'cat_id',
-                'slug',
-                'name',
-                'lng',
-                // 'lat',
+                [
+                    'attribute'     => 'town_id',
+                    'label'         => 'Город',
+                    'content'       => function ($data) {
+                        /**
+                         * @var $data \common\models\Places
+                         */
+                        if (isset($data->town->name)) {
+                            return $data->town->name;
+                        } else {
+                            return 'Город не задан';
+                        }
+                    },
+                    'headerOptions' => ['style' => 'width:200px']
+                ],
+                //'slug',
+    
+                [
+                    'attribute'     => 'name',
+                    'label'=>'Название',
+                    'content' => function ($data) {
+                        /**
+                         * @var $data \common\models\Places
+                         */
+                            return $data->name.'<br><b>LNG: </b>'.$data->lng.' <b>LAT:</b>'.$data->lat;
+                    },
+                    ],
+                /*'lng',
+                'lat',*/
+                /*[
+                      'attribute' => 'coords',
+                      'label' => 'Координаты',
+                      'content'=> function ($data) {
+                        /**
+                         * @var $data \common\models\Places
+                         */
+                        /*return $data->lng.'<br>'.$data->lat;
+                        },
+                ]*/
                 // 'title',
                 // 'meta_descr',
                 // 'short_descr:ntext',
